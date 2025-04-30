b2f6bed50095bd55f25cfba7111ceb9a99cd6208
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-04-29 23:46:43 -0400

    Add trending movies section and loading dynamic content

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,9 +7,18 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
+
   const {
     data: movies,
     loading: moviesLoading,
@@ -31,18 +40,31 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {moviesLoading
+        {moviesLoading || trendingLoading 
         ?(<ActivityIndicator size='large' color='#0000ff' className="mt-10 self-center"/>)
-        : moviesError?(<Text>Error: {moviesError?.message}</Text>)
+        : moviesError || trendingError?(<Text>Error: {moviesError?.message || trendingError?.message}</Text>)
         :
         <View className="flex-1 mt-5">
           <SearchBar
             onPress={() => router.push("/search")}
             placeholder="Search for a movie"
           />
+          {trendingMovies && (
+            <View className="mt-10">
+              <Text className="text-lg text-white font-bold mb-3">Trending movies</Text>
+            </View>
+          )}
           <>
-          <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
 
+          <FlatList className="mb-4 mt-3" data={trendingMovies} renderItem={({item,index})=>(
+            <TrendingCard movie={item} index={index}/>
+          )} keyExtractor={(item)=>item.movie_id.toString()}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            ItemSeparatorComponent={()=><View className="w-4"/>}
+          />
+
+          <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
           <FlatList data={movies}
             renderItem={({item})=>
               <MovieCard {...item}/>
