61983558468c4f2674931c6b426bc4dc59b64dab
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-06 22:21:52 -0400

    Basic page desing

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import { icons } from '@/constants/icons'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View className='flex-1 bg-primary items-center justify-center'>
+      <Image source={icons.person} className='w-10 h-10'/>
+      <Text className='text-white'>Profile</Text>
     </View>
   )
 }
