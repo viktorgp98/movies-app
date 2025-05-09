9990feb020346d3d499dd9f350c6a64f2bedc936
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-09 17:32:54 -0400

    Get the userData from logged user and show

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,55 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { icons } from '@/constants/icons'
+import { Snackbar } from 'react-native-paper'
+
+/* import Snackbar from 'react-native-snackbar' */
+import {AppWriteContext} from '@/components/AppWriteContext'
+
+type UserObj ={
+  name: string
+  email: string
+}
 
 const Profile = () => {
+  const [userData,setUserData] = useState<UserObj>()
+  const [visible,setVisible] = useState<boolean>(false)
+  const onDismissSnackBar = () => setVisible(false);
+  const {appwrite,setIsLoggedIn} = useContext(AppWriteContext)
+  
+  const handleLogout = async () => {
+    appwrite.logout()
+    .then(() => {
+      setIsLoggedIn(false)
+      return <Snackbar visible={visible} onDismiss={onDismissSnackBar} duration={3000}>Algo</Snackbar>;
+    })
+  }
+
+  useEffect(() => {
+    appwrite.getCurrentUser()
+    .then(response =>{
+      if (response) {
+        const user:UserObj = {
+          name: response.name,
+          email: response.email,
+        }
+        setUserData(user)
+      }
+    })
+    
+  }, [appwrite])
+  
+
   return (
     <View className='flex-1 bg-primary items-center justify-center'>
       <Image source={icons.person} className='w-10 h-10'/>
       <Text className='text-white'>Profile</Text>
+        {userData && (
+          <View>
+            <Text>Name: {userData.name}</Text>
+            <Text>Email: {userData.email}</Text>
+        </View>
+        )}
     </View>
   )
 }
